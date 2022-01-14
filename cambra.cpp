9c1fb1d75aad10582b7676b5576911bf4c1dfbbc
Alexandra <alexandra.espinosa@estudiantat.upc.edu>
2022-01-14 13:56:29 +0100

    Comentario cambra DONE

--- cambra.cpp
+++ cambra.cpp
@@ -1,32 +1,39 @@
 #include "cambra.hpp"
-#include <iostream>
 
-using namespace std;
-
-// Construeix una cambra amb les portes als llocs indicats.
-// bool n = false, s = false, e = false, o=false -> no hay puertas
-//si no hay puertas = no hay paredes?
-//enum {NORD, EST, SUD, OEST, NO_DIR};
+/* Construeix una cambra amb les portes als llocs indicats.*/
+/* COST: O(1) Ja que només hi han assignacions*/
 cambra::cambra(bool n, bool s, bool e, bool o) throw(error){
-  //0 --> nord, 1 --> sud, 2 --> est, 3 --> oest
-  _porta[0] = n; //NORD = n
-  _porta[1] = e; //EST = e
-  _porta[2] = s; //SUD = s
-  _porta[3] = o; //OEST = o
+    /*PRE: Ens passen per paràmetre 4 booleans tots inicilitzats a false*/
+    /*POST: Array complert amb portes tancades a totes les direccions*/
+
+  _porta[0] = n;
+  _porta[1] = e;
+  _porta[2] = s;
+  _porta[3] = o;
 }
 
 // Constructora per còpia, assignació i destructora.
-cambra::cambra(const cambra & c) throw(error){
 
-  _porta[0] = c._porta[0]; //NORD = n
+/* Constructora on donada una cambra, es fa una còpia*/
+/* COST: O(1) Ja que només hi han assignacions*/
+cambra::cambra(const cambra & c) throw(error){
+  /*PRE: Ens passen per paràmetre una cambra c*/
+  /*POST: Copiem la cambra per p.i a la nostra*/
+
+  _porta[0] = c._porta[0];
   _porta[1] = c._porta[1];
   _porta[2] = c._porta[2];
   _porta[3] = c._porta[3];
 }
 
+/* Assignació d'una cambra donada a la nostra*/
+/* COST: O(1) Ja que només hi han assignacions*/
 cambra & cambra::operator=(const cambra & c) throw(error){
+  /*PRE: Ens passen per paràmetre una cambra c*/
+  /*POST: Retorna l'assignació de la cambra passada per p.i a la nostra*/
+
   if(this != &c){
-    _porta[0] = c._porta[0]; //NORD = n
+    _porta[0] = c._porta[0];
     _porta[1] = c._porta[1];
     _porta[2] = c._porta[2];
     _porta[3] = c._porta[3];
@@ -34,26 +41,31 @@ cambra & cambra::operator=(const cambra & c) throw(error){
   return *this;
 }
 
+/* Destructora automàtica de la nostra cambra */
+/* COST: O(1)*/
 cambra::~cambra() throw(){}
 
-// Retorna si l'habitació té una porta oberta a la paret indicada.
-// oberta == true
-// En p nos dirá si es NORD,SUD,EST,OEST,NO_DIR
+/* Retorna si l'habitació té una porta oberta a la paret indicada.*/
+/* COST: O(1) Ja que només hi han assignacions i if's*/
 bool cambra::porta_oberta(paret p) const throw(){
+  /*PRE: Ens passen per paràmetre una direcció d'una paret*/
+  /*POST: Retorna un booleà true si la porta en la paret indicada és oberta
+  sinó retorna false*/
+
   bool porta_open = false;
-  if(p == 0) //NORD
+  if(p == 0)
   {
     porta_open = _porta[0];
   }
-  else if ( p == 1) //EST
+  else if ( p == 1)
   {
     porta_open = _porta[1];
   }
-  else if( p == 2) //SUD
+  else if( p == 2)
   {
     porta_open = _porta[2];
   }
-  else if ( p == 3) //OEST
+  else if ( p == 3)
   {
     porta_open = _porta[3];
   }
@@ -62,20 +74,26 @@ bool cambra::porta_oberta(paret p) const throw(){
 
 // Obre una nova porta a la paret indicada. Si la porta ja està oberta no
 // fa res. Es produeix un error si la paret és NO_DIR.
+/* COST: O(1) Ja que només hi han assignacions i if's*/
 void cambra::obre_porta(paret p) throw(error){
-  if(p == 0) //NORD
+  /*PRE: Ens passen per paràmetre una direcció d'una paret */
+  /*POST: Possem a true la porta indicada segons la direcció de la paret pasada per p.i en
+  cas que no hagi estat oberta abans, si no existeix la paret que ens passen, llancem
+  l'error ParetInexistent*/
+
+  if(p == 0)
   {
     _porta[0] = true;
   }
-  else if ( p == 1) //EST
+  else if ( p == 1)
   {
     _porta[1] = true;
   }
-  else if( p == 2) //SUD
+  else if( p == 2)
   {
     _porta[2] = true;
   }
-  else if ( p == 3) //OEST
+  else if ( p == 3)
   {
     _porta[3] = true;
 
@@ -88,20 +106,26 @@ void cambra::obre_porta(paret p) throw(error){
 
 // Tanca la porta a la paret indicada. Si la porta ja estava tancada no
 // fa res. Es produeix un error si la paret és NO_DIR.
+/* COST: O(1) Ja que només hi han assignacions i if's*/
 void cambra::tanca_porta(paret p) throw(error){
-  if( p == 0) //NORD
+  /*PRE: Ens passen per paràmetre una direcció d'una paret */
+  /*POST: Possem a false la porta indicada segons la direcció de la paret pasada per p.i en
+  cas que no hagi estat tancada abans, si no existeix la paret que ens passen, llancem
+  l'error ParetInexistent*/
+
+  if( p == 0)
   {
     _porta[0] = false;
   }
-  else if ( p == 1) //EST
+  else if ( p == 1)
   {
     _porta[1] = false;
   }
-  else if( p == 2) //SUD
+  else if( p == 2)
   {
     _porta[2] = false;
   }
-  else if ( p == 3) //OEST
+  else if ( p == 3)
   {
     _porta[3] = false;
   }else{
@@ -110,19 +134,32 @@ void cambra::tanca_porta(paret p) throw(error){
 
 }
 
-// Igualtat i desigualtat entre cambres. Dues cambres es consideren iguals
+// Igualtat entre cambres. Dues cambres es consideren iguals
 // si tenen les mateixes portes obertes a les mateixes parets.
+/* COST: O(1) Ja que només hi han assignacions i if's*/
 bool cambra::operator==(const cambra & c) const throw(){
+  /*PRE: Ens passen per paràmetre una cambra c*/
+  /*POST: Retorna true en cas que l'array de portes de la cambra passada per p.i
+  sigui igual a la nostra */
+
   bool igual = false;
   if((_porta[0] == c._porta[0]) &&
-      (_porta[1] == c._porta[1]) &&
-      ( _porta[2] == c._porta[2]) &&
-      ( _porta[3] == c._porta[3] )) igual = true;
+  (_porta[1] == c._porta[1]) &&
+  ( _porta[2] == c._porta[2]) &&
+  ( _porta[3] == c._porta[3] )) igual = true;
 
-return igual;
+  return igual;
 }
+
+// Desigultat entre cambres. Dues cambres no es consideren iguals
+// si no tenen les mateixes portes obertes a les mateixes parets.
+/* COST: O(1) Ja que és una igualació negada*/
 bool cambra::operator!=(const cambra & c) const throw(){
-    return not(*this == c);
+  /*PRE: Ens passen per paràmetre una cambra c*/
+  /*POST: Retorna false en cas que l'array de portes de la cambra passada per p.i
+  sigui diferent a la nostra */
+
+  return not(*this == c);
 }
 
 // Operador "menor que" entre cambres. Una cambra és més petita que una
@@ -133,18 +170,31 @@ bool cambra::operator!=(const cambra & c) const throw(){
 //   cambra c1, c2(true,true), c3(true, false, true);
 //   cout << (c1 < c2); // escriu 'true'
 //   cout << (c2 < c3); // escriu 'true'
+
+/* COST: O(4) És el nombre de posicions que té el array _porta*/
 bool cambra::operator<(const cambra & c) const throw(){
+  /*PRE: Ens passen per paràmetre una cambra c */
+  /*POST: Retorna true en cas que nostra cambra sigui més petita que la passada per p.i,
+  false en cas contrari*/
+
   int contT_nuestro = 0, contT_suyo = 0;
   bool menor_que = false;
-  bool parar = false;
+  bool parar = false; // variable sentinella per parar l'execució una vegada trobada una porta tancada
+
+  /* COST: O(4) És el nombre de posicions que té el array _porta*/
   for (int i = 0; i < 4; i++){
+    /*INV: Mentres que la i sigui menor a 4 recorrem l'array _porta*/
     if(c._porta[i] == false) contT_suyo++;
     if(_porta[i] == false) contT_nuestro++;
   }
+
   if(contT_nuestro > contT_suyo) menor_que = true;
   else if( contT_nuestro == contT_suyo){
     int i =0;
+    /* COST: O(4) És el nombre de posicions que té el array _porta */
     while(!parar &&  i < 4){
+      /*INV: Mentres el booleà parar no sigui true i la i sigui menor a 4,
+      anirem recorrent l'array _porta*/
       if(_porta[i] != c._porta[i]){
         if(_porta[i] == false) menor_que = true;
         parar = true;
--- laberint.cpp
+++ laberint.cpp
@@ -6,13 +6,24 @@ using namespace std;
 // Constructora d'un laberint buit sense excavar (sense cap porta oberta)
 // de la mida indicada. Totes les cambres del laberint no tenen cap porta.
 // Es produeix un error si el número de files o de columnes del laberint és 0.
+
+/* COST: O(posi.first) ja que com a màxim arriba fins al número de files indicat per
+paràmetre*/
 laberint::laberint(nat num_fil, nat num_col) throw(error){
+  /*PRE: En passen el nombre de files i columnes respectivament*/
+  /*POST: Creació dinàmica d'un laberint segons les mides passades per p.i
+  amb totes les portes tancades*/
+
   if (num_fil != 0 && num_col != 0)
   {
     posi.first = num_fil;
     posi.second = num_col;
     _c = new cambra *[posi.first];
+    /* COST: O(posi.first) ja que com a màxim arriba fins al número de files indicat per
+    paràmetre*/
     for(nat i = 0; i < posi.first; i++){
+      /*INV: Mentres que la i sigui menor a posi.first a la posició i de l'array _c,
+      anem creant un array amb les columnes del laberint*/
         _c[i] = new cambra [posi.second];
     }
   }
@@ -23,8 +34,12 @@ laberint::laberint(nat num_fil, nat num_col) throw(error){
 // Constructora d'un laberint a partir d'un istream (canal d'entrada) is.
 // El format de l'istream seguirà l'exposat a l'apartat 2.3. Es presuposa que
 // el laberint és correcte.
+/* COST: */
 laberint::laberint(std::istream & is) throw(error){
-	int fila = 0, columna = 0;
+  /*PRE: */
+  /*POST:*/
+
+  int fila = 0, columna = 0;
   is >> fila >> columna;
   posi.first=fila;
   posi.second=columna;
@@ -34,10 +49,13 @@ laberint::laberint(std::istream & is) throw(error){
 
   laberint l = laberint(posi.first, posi.second);
   int contador = -1;
+  /* COST: */
   while(getline(is, s)){
   int medida = s.size()-1;
   if(trobat){
+    /* COST: O(medida)*/
   	for (int i = 0, fil = 0; i < medida; i+=2, fil++){
+        /*INV:*/
   		if (s[i] != '*' ){
   			 l._c[contador][fil-1].obre_porta(paret("est"));
   			 l._c[contador][fil].obre_porta(paret("oest"));
@@ -47,7 +65,9 @@ laberint::laberint(std::istream & is) throw(error){
   	contador++;
   }
   else{
+    /* COST: O(medida)*/
   	for (int i = 1, fil = 0; i < medida; i+=2, fil++){
+        /*INV: Mentres que i sigui menor a medida */
   		if (s[i] != '*' ){
   			 l._c[contador][fil].obre_porta(paret("nord"));
          l._c[contador-1][fil].obre_porta(paret("sud"));
@@ -62,36 +82,55 @@ laberint::laberint(std::istream & is) throw(error){
 }
 
 // Constructora per còpia, assignació i destructora.
+/* COST: O(posi.first*posi.second) ja que recorrem totes les posicions del laberint l per total
+copiar-lo al nostre laberint*/
 laberint::laberint(const laberint & l) throw(error){
+  /*PRE: Ens passen un laberint l*/
+  /*POST:*/
+
   posi.first = l.posi.first;
   posi.second = l.posi.second;
   _c = new cambra* [posi.first];
+  /* COST: */
   for(nat i = 0; i < posi.first; i++){
+      /*INV:*/
     _c[i] = new cambra[posi.second];
   }
+  /* COST: */
   for(nat i = 0; i < posi.first; i++){
       for(nat j = 0; j < posi.second; j++){
+          /*INV:*/
         _c[i][j] = l._c[i][j];
       }
   }
 }
 
+/* COST: */
 laberint & laberint::operator=(const laberint & l) throw(error){
+  /*PRE: */
+  /*POST:*/
+
   if(this != &l){
     if(posi.first != l.posi.first ||  posi.second != l.posi.second){
+       /* COST: */
       for(nat i = 0; i < posi.first; i++){
-            delete[] _c[i];
+          /*INV:*/
+          delete[] _c[i];
       }
       delete[] _c;
     }
       posi.first = l.posi.first;
       posi.second = l.posi.second;
       _c = new cambra* [posi.first];
+      /* COST: */
       for(nat i = 0; i < posi.first; i++){
+          /*INV:*/
           _c[i] = new cambra[posi.second];
       }
+      /* COST: */
       for(nat i = 0; i < posi.first; i++){
           for(nat j = 0; j < posi.second; j++){
+              /*INV:*/
             _c[i][j] = l._c[i][j];
           }
       }
@@ -99,18 +138,31 @@ laberint & laberint::operator=(const laberint & l) throw(error){
   return *this;
 }
 
+/* COST: */
 laberint::~laberint() throw(){
+  /*PRE: */
+  /*POST:*/
+
   for(nat i = 0; i < posi.first; i++){
-        delete[] _c[i];
+      /*INV:*/
+      delete[] _c[i];
   }
   delete[] _c;
 }
 
 // Retornen el número de files i columnes que té el laberint, respectivament.
+/* COST: */
 nat laberint::num_files() const throw(){
+  /*PRE: */
+  /*POST:*/
+
   return posi.first;
 }
+/* COST: */
 nat laberint::num_columnes() const throw(){
+  /*PRE: */
+  /*POST:*/
+
   return posi.second;
 }
 
@@ -120,14 +172,21 @@ nat laberint::num_columnes() const throw(){
 //   posicio pos(2,2);  --> Posicion que accedemos dentro del laberinto que hemos creado previamente
 //   cambra c = l(pos); --> Se nombra la cabmra en tal pos.
 // Es produeix un error si la posició donada no existeix al laberint.
+
+/* COST: */
 cambra laberint::operator()(const posicio & pos) const throw(error){
+  /*PRE: */
+  /*POST:*/
+
   int fila =  (int) pos.first-1;
   int col =  (int) pos.second-1;
 
   cambra res;
   if(fila >= 0 && fila < (int)posi.first && col >= 0 && col < (int)posi.second){
+    /* COST: */
     for(int i = 0; i < (int)posi.first; i++){
         for(int j = 0; j < (int)posi.second; j++){
+            /*INV:*/
             if(_c[i][j] == _c[fila][col]){
               res = _c[fila][col];
             }
@@ -144,12 +203,19 @@ cambra laberint::operator()(const posicio & pos) const throw(error){
 // També obre la porta corresponent en la cambra adjacent. Es produeix un error
 // si la posició no existeix o no es pot obrir una porta en la direcció
 // indicada perquè dóna a l'exterior.
+
+/* COST: */
 void laberint::obre_porta(paret p, const posicio & pos) throw(error){ //REVISAR CONDICIONES
+  /*PRE: */
+  /*POST:*/
+
   int fila = (int)pos.first-1;
   int col = (int)pos.second-1;
   if(fila >= 0 && fila < (int)posi.first && col >= 0 && col < (int)posi.second){
+    /* COST: */
     for(int i = 0; i < (int)posi.first; i++){
         for(int j = 0; j < (int)posi.second; j++){
+          /*INV:*/
           //BORDES
            if(fila == 0 && p == 0) throw error(PortaExterior);
            else if(fila == (int)posi.first-1 && p == 2) throw error(PortaExterior);
@@ -176,13 +242,19 @@ void laberint::obre_porta(paret p, const posicio & pos) throw(error){ //REVISAR 
 // Tanca la porta en la paret donada de la cambra que està especificada per pos.
 // També tanca la porta corresponent en la cambra adjacent. Es produeix un error
 // si la posició no existeix.
+
+/* COST: */
 void laberint::tanca_porta(paret p, const posicio & pos) throw(error){
+  /*PRE: */
+  /*POST:*/
+
   int fila = (int)pos.first-1;
   int col = (int)pos.second-1;
   if(fila >= 0 && fila < (int)posi.first && col >= 0 && col < (int)posi.second){
+     /* COST: */
     for(int i = 0; i < (int)posi.first; i++){
         for(int j = 0; j < (int)posi.second; j++){
-
+            /*INV:*/
             if( i == fila && j == col){
                _c[i][j].tanca_porta(p);
                //parets adjacents
@@ -201,26 +273,32 @@ void laberint::tanca_porta(paret p, const posicio & pos) throw(error){
 
 // Escriu el laberint a l'ostream (canal de sortida) os. El format per escriure
 // el laberint seguirà l'exposat a l'apartat 2.3.
+
+/* COST: */
 void laberint::print(std::ostream & os) const throw(){
+    /*PRE: */
+    /*POST:*/
 
-os<< (int)posi.first<<" "<<(int)posi.second<<'\n';
-for (int i = 0; i < (int)posi.first; i++){
-	for(int j=0; j<(int)posi.second; j++){
-	   os << '*' << (_c[i][j].porta_oberta(paret("nord")) ? ' ' : '*');
-	}
-	os<<'*'<<'\n'<< (_c[i][0].porta_oberta(paret("oest")) ? ' ' : '*') << ' ';
+    os<< (int)posi.first<<" "<<(int)posi.second<<'\n';
+    /* COST: */
+    for (int i = 0; i < (int)posi.first; i++){
+    	for(int j=0; j<(int)posi.second; j++){
+           /*INV:*/
+    	   os << '*' << (_c[i][j].porta_oberta(paret("nord")) ? ' ' : '*');
+    	}
 
-	for(int j=0; j<(int)posi.second; j++){
-    if(j == (int)posi.second-1){
-      os << (_c[i][j].porta_oberta(paret("est")) ? ' ' : '*');
-    }else{
-       os << (_c[i][j].porta_oberta(paret("est")) ? ' ' : '*') << ' ';
-     }
-	}
-	os<<'\n';
+    	os<<'*'<<'\n'<< (_c[i][0].porta_oberta(paret("oest")) ? ' ' : '*') << ' ';
 
-
-}
+    	for(int j=0; j<(int)posi.second; j++){
+            /*INV:*/
+            if(j == (int)posi.second-1){
+              os << (_c[i][j].porta_oberta(paret("est")) ? ' ' : '*');
+            }else{
+               os << (_c[i][j].porta_oberta(paret("est")) ? ' ' : '*') << ' ';
+             }
+    	}
+    	os<<'\n';
+    }
 
  os<< string((int)posi.second*2+1, (_c[(int)posi.first-1][(int)posi.second-1].porta_oberta(paret("sud")) ? ' ' : '*')) <<'\n';
 
