6e9e307f4a8b78bb34b7f012eb57e92debed73ec
Denise <60241665+denisenavarro@users.noreply.github.com>
2022-01-16 10:06:32 +0100

    Docu dedalus empezada

--- dedalus.cpp
+++ dedalus.cpp
@@ -1,14 +1,28 @@
 #include "dedalus.hpp"
 
-// Excava el laberint, fent un laberint perfecte. Un laberint està excavat
-// si i només si alguna de les seves cambres té alguna porta oberta.
-// Es produeix un error si el laberint ja està excavat.
+/** CLASSE DEDALUS **/
+/*
+    Aquest mòdul té com a objectiu definir el namespace dedalus el qual conté la funció
+    construir. Aquesta funció cal que tingui la capacitat de generar laberints del sistema
+    MAZE. Aquesta funcionalitat es desenvolupa a partir del laberint que rep per paràmetre i
+    excava fins adquir un laberint que sigui perfecte. Considerem laberint perfecte tot aquell
+    que no tingui àrees inaccessibles com cambres tancades ni tampoc es formin cicles.
+
+*/
+
+
 util::Random R;
 
+/* COST: */
 int generaparetRandom(bool exterior, posicio prand, laberint & M){
+  /*PRE: Ens passen un booleà exterior inicialitzat a true, una posició random i el laberint M que ens passen
+  a la funció construir */
+  /*POST: Retorna un integer corresponent a una paret random generada i que no pertany a l'exterior del
+  laberint*/
   int rparet = R(0, 3);
   int p = rparet;
   while(exterior){
+    /*INV: Mentre exterior no sigui false continua generant parets randoms*/
     if((prand.first == 1 && p == 0 )|| (prand.first == M.num_files() && p == 2)
     || (prand.second == 1 && p == 3 ) || (prand.second == M.num_columnes() && p == 1)){
      rparet = R(0, 3);
@@ -21,14 +35,23 @@ int generaparetRandom(bool exterior, posicio prand, laberint & M){
     return p;
 }
 
+/*COST: */
 posicio generaposicioRandom(laberint & M){
+  /*PRE: Ens passen el laberint M que ens ve donat a la funció construir */
+  /*POST: Retorna una posició random entre 1 i el número de files i número de
+  columnes respectivament*/
   nat rfila = R(1,M.num_files());
   nat rcol =  R(1,M.num_columnes());
   posicio posi(rfila, rcol);
   return posi;
 }
 
+/*COST: O(1) ja que només es realitzen if's i assignacions*/
 posicio generaAdjacent(posicio prand, int pared){
+  /*PRE: Ens passen una posició random que hem generat prèviament i una paret
+  random pertanyent a la cambra que hi ha en la posició prand*/
+  /*POST: Retorna una posició equivalent a l'adjacent de prand i que comparteixin
+  la paret pasada per paràmetre*/
   posicio paux = prand;
   if(pared == 0) paux.first = prand.first-1; //NORD
   else if( pared == 1) paux.second = prand.second+1; //EST
@@ -37,7 +60,10 @@ posicio generaAdjacent(posicio prand, int pared){
   return paux;
 }
 
-
+// Excava el laberint, fent un laberint perfecte. Un laberint està excavat
+// si i només si alguna de les seves cambres té alguna porta oberta.
+// Es produeix un error si el laberint ja està excavat.
+/*COST: */
 void dedalus::construir(laberint & M) throw(error){
 
     nat tamanyL = M.num_files() * M.num_columnes();
